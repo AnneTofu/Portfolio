e2361287f81ce2d688797cc95e5e9b447a7ab3e5
Anne-Sophie Drozak <107859164+AnneTofu@users.noreply.github.com>
2022-06-27 09:38:38 +0200

    Sprint 3

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hello World!");
+let chat="Hochi";
+console.log(chat);
+
+chat="Busan";
+console.log(chat);
+
+
+let header=document.getElementById("chat");
+console.log(header);
+header.addEventListener("click",()=>{
+    console.log("click")
+});
